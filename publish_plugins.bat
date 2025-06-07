cf495c56d2eb261f88d4bbdb838f9c24e98ff8f8
sibber5 <ghasjado@gmail.com>
2025-06-07 22:57:45 +0300

    Update folder names and README and fix publish_plugins.bat

--- publish_plugins.bat
+++ publish_plugins.bat
@@ -9,6 +9,6 @@ rmdir "./ExtractSearchIndexEx/bin/Release/net8.0/publish" /s /q
 dotnet clean ./ExtractSearchIndexEx/Sibber.Docfx.ExtractSearchIndexEx.csproj -c Release
 dotnet publish ./ExtractSearchIndexEx/Sibber.Docfx.ExtractSearchIndexEx.csproj -c Release
 
-xcopy "./ExtractSearchIndexEx/bin/Release/net8.0/publish" "./docfx/templates/custom_template/plugins" /v /s /e /h /y
+xcopy "./ExtractSearchIndexEx/bin/Release/net8.0/publish" "./docs/templates/customizations/plugins" /v /s /e /h /y
 
 if "%1" NEQ "--no-pause" pause
