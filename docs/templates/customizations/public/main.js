2273f8c1952c93611291ccc6ad23197f5e1395cc
sibber <ghasjado@gmail.com>
2025-06-18 21:17:53 +0300

    Initial skeleton

---
+++ docs/templates/customizations/public/main.js
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 sibber (GitHub: sibber5)
+
+export default {
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/x/y',
+            title: 'GitHub'
+        },
+        {
+            icon: 'box-seam-fill',
+            href: 'https://www.nuget.org/packages/z',
+            title: 'NuGet'
+        }
+    ]
+}
